659e6d8c830c731c85886076355a133fad30224b
Ageng Hermawan <ageng@monstera.id>
2025-05-31 01:27:09 +0700

    Update CI CD

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     images: {
         unoptimized: true,
     },
-    output: 'export', // Tambahan untuk static export
+
 };
 
 export default nextConfig;
