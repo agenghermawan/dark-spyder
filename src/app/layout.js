0185948471eade0724df050d99a28a0e5fc4e438
ageng <ageng@monstera.id>
2025-08-29 19:18:11 +0700

    Imporvement functional test part 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,19 +2,14 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
 import { AuthProvider } from "../context/AuthContext";
+import LoaderWrapper from "../components/ui/loader-wrapper";
 
-const poppins = Poppins({
-    variable: "--font-poppins",
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-});
+const poppins = Poppins({ variable: "--font-poppins", subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export const metadata = {
     title: "Clandestine Project",
     description: "Clandestine Project.",
-    icons: {
-        icon: "image/favicon.ico",
-    },
+    icons: { icon: "image/favicon.ico" }
 };
 
 export default function RootLayout({ children }) {
@@ -23,7 +18,9 @@ export default function RootLayout({ children }) {
         <body className="antialiased">
         <AuthProvider>
             <Navbar />
-            {children}
+            <LoaderWrapper>
+                {children}
+            </LoaderWrapper>
         </AuthProvider>
         </body>
         </html>
--- src/components/ui/loader-wrapper.js
+++ src/components/ui/loader-wrapper.js
@@ -1,17 +1,9 @@
-'use client';
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+'use client'
+import { useAuth } from "../../context/AuthContext";
 import SlideLoader from "../../components/ui/loader-page";
 
 export default function LoaderWrapper({ children }) {
-    const [isLoading, setIsLoading] = useState(false);
-    const pathname = usePathname();
-
-    useEffect(() => {
-        setIsLoading(true);
-        const timeout = setTimeout(() => setIsLoading(false), 1000);
-        return () => clearTimeout(timeout);
-    }, [pathname]);
-
-    return isLoading ? <SlideLoader /> : children;
+    const { authState } = useAuth();
+    if (authState === "loading") return <SlideLoader />;
+    return children;
 }
